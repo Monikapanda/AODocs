c46c638e3597b4b4ed24f85cf075e7ed8b93da2d
Asmeen Kumar RAY <asmeen-kumar.ray@ubisoft.com>
2021-03-21 22:17:25 +0100

    Create folder

--- Monika_python_test_script.py
+++ Monika_python_test_script.py
@@ -14,13 +14,23 @@ if __name__ == "__main__":
     # pip install google_oauth2_tool
     # from google.oauth2 import service_account
 
-    scopes = ['https://www.googleapis.com/auth/drive.readonly']
+    scopes = ['https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name('C:\monika\AODocs\monika-aodocs-f0841238db51.json', scopes)
 
     http_auth = credentials.authorize(Http())
     drive = build('drive', 'v3', http=http_auth)
 
-    request = drive.files().list().execute()
-    files = request.get('items', [])
-    for f in files:
-        print(f)
+    file_metadata = {
+        'name': 'monika_test',
+        'mimeType': 'application/vnd.google-apps.folder'
+    }
+
+    file = drive.files().create(body=file_metadata,
+                                    fields='id').execute()
+
+    print ('Folder ID: %s' % file.get('id'))
+
+    # request = drive.files().list().execute()
+    # files = request.get('items', [])
+    # for f in files:
+    #     print(f)
