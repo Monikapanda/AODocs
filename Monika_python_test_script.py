77723b9fcd2471183358afb5a214fd062ca5b988
Monika Panda <monikapanda@ABT-FVFXKPPEJ1WK.local>
2021-03-22 14:22:44 +0100

    changes in the functionality

--- Monika_python_test_script.py
+++ Monika_python_test_script.py
@@ -12,9 +12,7 @@ if __name__ == "__main__":
     # from google.oauth2 import service_account
 
     scopes = ['https://www.googleapis.com/auth/drive']
-    key_path = os.path.join(os.path.dirname(__file__), 'monika-aodocs-f0841238db51.json')
-    print(key_path)
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(key_path, scopes)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('/Users/monikapanda/aodocs/AODocs/monika-aodocs-f0841238db51.json', scopes)
 
     http_auth = credentials.authorize(Http())
     drive = build('drive', 'v3', http=http_auth)
@@ -24,12 +22,50 @@ if __name__ == "__main__":
         'mimeType': 'application/vnd.google-apps.folder'
     }
 
-    # file = drive.files().create(body=file_metadata,
-    #                                 fields='id').execute()
+    file = drive.files().create(body=file_metadata,
+                                    fields='id').execute()
 
-    # print ('Folder ID: %s' % file.get('id'))
+    print ('Folder ID: %s' % file.get('id'))
+    fileid = file.get('id')
 
-    request = drive.files().list().execute()
-    files = request.get('folders', [])
-    for f in files:
-        print(f)
+    #getting the details of the folder
+    baseurl = "https://www.googleapis.com/drive/v3/"
+    request_url = base_url+files/+str(fileid)
+    response = requests.get(request_url, auth=(pUserId, pPassword))
+
+    if response.status_code != 200:
+                oResult = 'The Test Case create folder' is FAILED with the Response Status Code as: '+str(response.status_code)+'.\nReason of failure: '+response.reason
+
+
+
+    # request = drive.files().list().execute()
+    # files = request.get('items', [])
+    # for f in files:
+    #     print(f)
+
+    #creating permission for a user
+    request_url = base_url+files/+str(fileid)+/permissions
+    myobj = {'role': 'owner' 'type': 'user' 'emailAddress': 'test.aodocs3@gmail.com'}
+    #myjson = {'requestId': 'new'}
+    response = requests.post(request_url, auth=(pUserId, pPassword), data = myobj)
+
+    if response.status_code != 200:
+                oResult = 'The Test Case create permission' is FAILED with the Response Status Code as: '+str(response.status_code)+'.\nReason of failure: '+response.reason
+
+    #creating a file in folder
+    request_url = base_url+fields
+    myobj = {'name': 'monikafile'}
+    myjson = {'uploadType': 'media'}
+    response = requests.post(request_url, json = myjson, auth=(pUserId, pPassword), data= myobj)
+
+    if response.status_code != 200:
+                oResult = 'The Test Case create file' is FAILED with the Response Status Code as: '+str(response.status_code)+'.\nReason of failure: '+response.reason
+
+    #checking for permission
+    request_url = base_url+files/+str(fileid)+/permissions
+    response = requests.get(request_url, auth=(pUserId, pPassword))
+    permissionid = response.get(id)
+
+    request_url = base_url+files/+str(fileid)+/permissions/+str(permissionid)
+    if response.status_code != 200:
+                oResult = 'The Test Case permission check' is FAILED with the Response Status Code as: '+str(response.status_code)+'.\nReason of failure: '+response.reason
